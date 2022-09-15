001bc8e91e05b86cc46c34af5919f6fa6c693a21
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-09-16 02:01:47 +0500

    header complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
-import { FaSearch, FaFacebook,FaHome,FaUserFriends,FaStore } from "react-icons/fa";
+import {FaBell, FaSearch, FaFacebook,FaHome,FaUserFriends,FaStore,FaChessBoard,FaFacebookMessenger } from "react-icons/fa";
 import { MdOutlinePersonalVideo } from "react-icons/md";
-
+import { TbGridDots } from "react-icons/tb";
 const header = () => {
 	return (
 		<div className="header">
@@ -18,33 +18,26 @@ const header = () => {
             <FaUserFriends className="hsb-3-icon"/>
             <MdOutlinePersonalVideo className="hsb-3-icon"/>
             <FaStore className="hsb-3-icon"/>
-
-            <i className="fa-solid fa-store "></i>
-            <i className="fa-solid fa-chess-board "></i>
+            <FaChessBoard className="hsb-3-icon"/>
         </div>
 
         <div className="hsb-4 ">
 
-            <div type="" className="dp">
-            <img src="./imgs/my-dp_1_50x50.jpg" alt="twitter DP" width={50} />
-                {/* <img src="" alt="" className=""/> */}
-                <p>Saad</p>
-            </div>
 
             <div className="app-icon">
-                <i className="fa-solid fa-braille "></i>
+                <TbGridDots className="icon"/>
             </div>
 
             <div className="msg-icon">
-                <i className="fa-brands fa-facebook-messenger "></i>
+            <FaFacebookMessenger className="icon"/>
             </div>
 
             <div className="bell-icon">
-                <i className="fa-solid fa-bell "></i>
+            <FaBell className="icon"/>
             </div>
-
-            <div className="drop-icon">
-                <i className="fa-solid fa-caret-down"></i>
+            
+            <div type="" className="dp">
+            <img src="./imgs/dp.jpg" alt="twitter DP" width={20} />
             </div>
 
         </div>
