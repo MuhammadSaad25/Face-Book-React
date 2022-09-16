ac8ae9bec963188e323b19b9e2230711b1f7c567
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-09-16 05:12:27 +0500

    almost complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Header from './components/header';
+import LeftBar from './components/leftBar';
+import Storybox from './components/storybox';
+import RightBar from './components/rightBar';
+import Content from './components/content';
 // import Sidebar from './components/sidebar';
 // import Menubar from './components/menubar';
 
 const App = () => {
   return (
     <div className="App">
-      <Header/>
-      {/* <Sidebar /> */}
+      <Header />
+
+      <div className='body'>
+        <LeftBar />
+        <div className='bsb-2p'>
+          <div className='bsb-2'>
+            <Storybox />
+            <Content />
+          </div>
+        </div>
+        <RightBar />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/rightBar.js
@@ -0,0 +1,66 @@
+// import { FaBell, FaSearch, FaFacebook, FaHome, FaUserFriends, FaStore, FaChessBoard, FaFacebookMessenger } from "react-icons/fa";
+// import { MdOutlinePersonalVideo } from "react-icons/md";
+// import { TbGridDots } from "react-icons/tb";
+const rightBar = () => {
+    return (
+        <div className="bsb-3">
+
+        <div className="contact">
+            <p>Contacts
+                <i className="fa-solid fa-video v-icon"></i>
+                <i className="fa-solid fa-magnifying-glass "></i>
+                <i className="fa-solid fa-ellipsis"></i>
+            </p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-1_28x28.jpg" alt="" className=""/>
+            <p>Shëìkh Mühämmâd Ärëéb (شیخ)</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-2_28x28.jpg" alt="" className=""/>
+            <p>Muhammad Bilal Aqeel (محمد بلال عقیل)</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-3_28x28.jpg" alt="" className=""/>
+            <p>Zoomy Khan</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-1_28x28.jpg" alt="" className=""/>
+            <p>Shëìkh Mühämmâd Ärëéb (شیخ)</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-2_28x28.jpg" alt="" className=""/>
+            <p>Muhammad Bilal Aqeel (محمد بلال عقیل)</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-3_28x28.jpg" alt="" className=""/>
+            <p>Zoomy Khan</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-1_28x28.jpg" alt="" className=""/>
+            <p>Shëìkh Mühämmâd Ärëéb (شیخ)</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-2_28x28.jpg" alt="" className=""/>
+            <p>Muhammad Bilal Aqeel (محمد بلال عقیل)</p>
+        </div>
+
+        <div type="" className="bowling-icon">
+            <img src="./imgs/cp-3_28x28.jpg" alt="" className=""/>
+            <p>Zoomy Khan</p>
+        </div>
+
+    </div>
+
+                );
+}
+
+                export default rightBar;
